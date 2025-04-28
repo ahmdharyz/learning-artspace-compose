44489366b15a459ff967806e3a1d188a6fc076f9
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-04-28 22:24:34 +1000

    Create index and add when condition for image, title, artist, year

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -51,7 +51,93 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceApp(modifier: Modifier = Modifier) {
-    var image = R.drawable.girl_with_a_pearl_earring_from_johannes_vermeer
+    var index by remember { mutableStateOf(0) }
+    var image: Int
+    var title: Int
+    var artist: Int
+    var year: Int
+
+    when (index) {
+        1 -> {
+            image = R.drawable._1_girl_with_a_pearl_earring__restored_version_1994____johannes_vermeer
+            title = R.string.title_1
+            artist = R.string.artist_1
+            year = R.string.year_1
+        }
+        2 -> {
+            image = R.drawable._2_avenue_in_giverny_or_garden_path_at_giverny___claude_monet
+            title = R.string.title_2
+            artist = R.string.artist_2
+            year = R.string.year_2
+        }
+        3 -> {
+            image = R.drawable._3_the_bookworm_from_carl_spitzweg
+            title = R.string.title_3
+            artist = R.string.artist_3
+            year = R.string.year_3
+        }
+        4 -> {
+            image = R.drawable._4_cafe_terrace_at_night_from_vincent_van_gogh
+            title = R.string.title_4
+            artist = R.string.artist_4
+            year = R.string.year_4
+        }
+        5 -> {
+            image = R.drawable._5_almond_blossoms_from_vincent_van_gogh
+            title = R.string.title_5
+            artist = R.string.artist_5
+            year = R.string.year_5
+        }
+        6 -> {
+            image = R.drawable._6_the_kiss_from_gustav_klimt
+            title = R.string.title_6
+            artist = R.string.artist_6
+            year = R.string.year_6
+        }
+        7 -> {
+            image = R.drawable._7_the_starry_night_from_vincent_van_gogh
+            title = R.string.title_7
+            artist = R.string.artist_7
+            year = R.string.year_7
+        }
+        8 -> {
+            image = R.drawable._8_the_starry_night_from_vincent_van_gogh
+            title = R.string.title_8
+            artist = R.string.artist_8
+            year = R.string.year_8
+        }
+        9 -> {
+            image = R.drawable._9_the_great_wave_of_kanagawa_from__katsushika_hokusai
+            title = R.string.title_9
+            artist = R.string.artist_9
+            year = R.string.year_9
+        }
+        10 -> {
+            image = R.drawable._10_path_in_the_wheat_at_pourville_from_claude_monet
+            title = R.string.title_10
+            artist = R.string.artist_10
+            year = R.string.year_10
+        }
+        11 -> {
+            image = R.drawable._11_impression_rising_sun_from_claude_monet
+            title = R.string.title_11
+            artist = R.string.artist_11
+            year = R.string.year_11
+        }
+        12 -> {
+            image = R.drawable._12_wanderer_above_the_sea_of_fog_by_caspar_david_friedrich
+            title = R.string.title_12
+            artist = R.string.artist_12
+            year = R.string.year_12
+        }
+        else -> {
+            image = R.drawable._1_girl_with_a_pearl_earring__restored_version_1994____johannes_vermeer
+            title = R.string.title_1
+            artist = R.string.artist_1
+            year = R.string.year_1
+        } // might need to change this
+    }
+
     var painterResource by remember { mutableStateOf(image) }
 
     Column(
